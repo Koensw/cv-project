680fe3586f2bd3fe50b3553a47764154a177c8e5
Koen Wolters <koen.wolters@cern.ch>
2018-04-27 01:40:54 +0200

    add prepare

---
+++ custom-tuple/networks/kitti/prepare_kitty.py
@@ -0,0 +1,34 @@
+from __future__ import print_function
+import numpy as np
+
+diff = 10
+
+min_nr = 0
+max_nr = 153
+
+min_nr += diff
+max_nr -= diff
+
+array = np.arange(min_nr, max_nr+1)
+
+labels_file = open("kitti_image_labels.txt", 'w')
+keys_file = open("kitti_image_keys.txt", 'w')
+
+base_name = "/srv/glusterfs/patilv/Datasets/kitti/raw/extracted"
+local_file_name = "2011_09_26_drive_0005_sync/image_02/data/"
+
+for i in range(rounds):
+    out = np.random.choice(array, 3, replace = False)
+    outsor = np.sort(out)
+    outsorrev = outsor[::-1]
+    
+    for idx in out:
+        keys_file.write("{}{:010}.png".format(local_file_name, idx))
+        keys_file.write(" ")
+    
+    if np.all(outsor == out) or np.all(outsorrev == out):
+        labels_file.write("{}\n".format(1))
+        print(out, "sorted")
+    else:
+        labels_file.write("{}\n".format(0))
+        print(out, "not sorted")
