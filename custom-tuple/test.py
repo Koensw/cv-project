265e44679b5182da6e4874e5161eafa2037e93ea
Koen Wolters <koen.wolters@cern.ch>
2018-04-30 10:07:26 +0200

    add test script

---
+++ custom-tuple/test.py
@@ -0,0 +1,43 @@
+from __future__ import print_function
+import sys
+if len(sys.argv) != 3:
+    print('ERROR: pass network prototxt and caffemodel as argument')
+    sys.exit(1)
+
+print("Loading libraries")
+
+import matplotlib
+#matplotlib.use("Agg")
+
+import numpy as np
+import os
+import caffe
+
+## ADD PATHS
+def addPath(path):
+    if path not in sys.path:
+        sys.path.insert(0, path)
+	#path = os.path.join(os.path.dirname(os.path.abspath(__file__)), path)
+	#sys.path.append(path)
+        print(('added {}'.format(path)))
+
+addPath('.')
+addPath('./layers')
+
+caffe.set_mode_cpu()
+
+## BUILD NET
+print("Building net")
+
+net = caffe.Net(sys.argv[1], sys.argv[2], caffe.TEST)
+net.forward()
+
+expout = np.vstack((net.blobs['label'].data, net.blobs['prob'].data[:,1])).transpose()
+
+np.set_printoptions(precision=2)
+print('Loss:', net.blobs['loss'].data)
+print('Exp / Out:\n', expout)
+
+print("Done")
+
+        
